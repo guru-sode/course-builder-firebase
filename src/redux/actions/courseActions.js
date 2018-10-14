56e03c9790dce98c25b27305aa5f3c91a113cbbd
Pavan Mahawar <pavan.mahawar@mountblue.io>
2018-10-14 16:16:46 +0530

    add createCourse action

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,3 +1,17 @@
-export const createCourse = (courser) => {
-    return;
+import { CREATE_COURSE } from '../../constants/actionTypes';
+
+export const createCourse = (course) => {
+
+    /*   return function to redux-thunk */
+    return (dispatch, { getFirebase }) => {
+        /* asyn call to firbase  to add course object with id to courses */
+        const firbase = getFirebase();
+        firbase.database().ref(`app/courses/${course.id}`).set(course)
+            .then(() => {
+                return dispatch({ type: CREATE_COURSE, payload: course });
+            }).catch((err) => {
+                console.log(err);
+            });
+    };
 };
+
