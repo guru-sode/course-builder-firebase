5530f3235b5bdb13471c166bf66e05d1c0ac0dda
Pavan Mahawar <pavan.mahawar@mountblue.io>
2018-10-14 16:00:17 +0530

    enhance store with reactReduxFirebase

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import firebaseConfig from '../config/firebaseConfig';
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const createStoreWithMiddleware = compose(
+    applyMiddleware(thunkMiddleware.withExtraArgument({ getFirebase })),
+    reactReduxFirebase(firebaseConfig)
+)(createStore);
+
 const store = createStoreWithMiddleware(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 export default store;
