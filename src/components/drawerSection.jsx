c4436f856e91d77127393bd2c4e50d2305d83233
guru-sode <gurukiran.sd@mountblue.io>
2018-10-15 11:39:03 +0530

    Modify

--- src/components/drawerSection.jsx
+++ src/components/drawerSection.jsx
@@ -12,62 +12,62 @@ const mailFolderListItems=['Intro'];
 const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: '625px',
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    minWidth: 0, // So the Typography noWrap works
-    height: 'auto',
-  },
-  toolbar: theme.mixins.toolbar,
+    root: {
+        flexGrow: 1,
+        height: '1000px',
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        minWidth: 0, // So the Typography noWrap works
+        height: 'auto',
+    },
+    toolbar: theme.mixins.toolbar,
 });
 
 function DrawerSection(props) {
-  const { classes } = props;
+    const { classes } = props;
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="absolute" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
+    return (
+        <div className={classes.root}>
+            <AppBar position="absolute" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
             Add section
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>{mailFolderListItems}</List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <AddSection />
-      </main>
-    </div>
-  );
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.toolbar} />
+                <List>{mailFolderListItems}</List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <AddSection />
+            </main>
+        </div>
+    );
 }
 
 DrawerSection.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(DrawerSection);
