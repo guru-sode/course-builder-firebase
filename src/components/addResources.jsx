2fbbd7ab06b2de0fc30e7b606b5073096d1a9e3a
guru-sode <gurukiran.sd@mountblue.io>
2018-10-16 10:52:21 +0530

    Fix bottom border in add section

--- src/components/addResources.jsx
+++ src/components/addResources.jsx
@@ -84,6 +84,7 @@ class AddResources extends Component {
                 />
                 <TextField
                     id="url"
+                    multiline={true}
                     label="Resource URL"
                     className={classes.textField}
                     type="url"
