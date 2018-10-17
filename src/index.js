c832db765039ae507014deb53f1703445e347f60
Pavan Mahawar <pavan.mahawar@mountblue.io>
2018-10-17 11:41:55 +0530

    modify store

--- src/index.js
+++ src/index.js
@@ -5,21 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    // <Provider store={store}>
-    //     <BrowserRouter>
-    //         <div>
-    //             <Route path="/" component={HomeNavbar} />
-    //             <Route exact path="/login" component={Login} />
-    //             <Route exact path="/signup" component={SignUp} />
-    //         </div>
-    //     </BrowserRouter>
-    // </Provider >
-    , document.getElementById('root'));
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+        , document.getElementById('root'));
+    serviceWorker.unregister();
+});
 
-serviceWorker.unregister();
+
