2dbf3cd642deadee732814c10eb61fc9b242d168
Pavan Mahawar <pavan.mahawar@mountblue.io>
2018-10-18 16:48:08 +0530

    made change in sectioAction.js

--- src/redux/actions/sectionActions.js
+++ src/redux/actions/sectionActions.js
@@ -23,136 +23,136 @@ export const addSection = (section) => {
             plan_of_attack: ''
         };
         const firebase = getFirebase();
-        firebase.database().ref(`app/sections/${section.sid}`).set(section)
-            .then(() => {
-                firebase.database().ref(`app/courses/${cid}`).update({
-                    section: [...currentCourse.section, sid]
-                }).then(() => {
-                    return dispatch({ type: ADD_SECTION_ID, payload: section.sid });
-                });
+        //     firebase.database().ref(`app/sections/${section.sid}`).set(section)
+        //         .then(() => {
+        //             firebase.database().ref(`app/courses/${cid}`).update({
+        //                 section: [...currentCourse.section, sid]
+        //             }).then(() => {
+        //                 return dispatch({ type: ADD_SECTION_ID, payload: section.sid });
+        //             });
 
+        //         }).catch((err) => {
+        //             return err;
+        //         });
+        // };
+    };
+
+    export const addVideo = (videoInfo) => {
+        /*   return function to redux-thunk */
+        return (dispatch, getState, { getFirebase }) => {
+            /* asyn call to firbase  to add course object with id to courses */
+            const store = getState();
+            const app = store.firebase.data.app;
+            const sid = store.sections.current_section;
+            const currentSection = app.sections[sid];
+            const firebase = getFirebase();
+            firebase.database().ref(`app/sections/${currentSection.sid}`).update({
+                resourses: [...currentSection.resourses, videoInfo]
+            }).then(() => {
+                return dispatch({ type: ADD_VIDEO, payload: videoInfo });
             }).catch((err) => {
                 return err;
             });
-    };
-};
-
-export const addVideo = (videoInfo) => {
-    /*   return function to redux-thunk */
-    return (dispatch, getState, { getFirebase }) => {
-        /* asyn call to firbase  to add course object with id to courses */
-        const store = getState();
-        const app = store.firebase.data.app;
-        const sid = store.sections.current_section;
-        const currentSection = app.sections[sid];
-        const firebase = getFirebase();
-        firebase.database().ref(`app/sections/${currentSection.sid}`).update({
-            resourses: [...currentSection.resourses, videoInfo]
-        }).then(() => {
-            return dispatch({ type: ADD_VIDEO, payload: videoInfo });
-        }).catch((err) => {
-            return err;
-        });
-    };
-};
-
-
-
-export const addAdditionalResourse = (resoursesInfo) => {
-    /*   return function to redux-thunk */
-    return (dispatch, getState, { getFirebase }) => {
-        /* asyn call to firbase  to add course object with id to courses */
-        const store = getState();
-        const app = store.firebase.data.app;
-        const sid = store.sections.current_section;
-        const currentSection = app.sections[sid];
-        const firebase = getFirebase();
-        firebase.database().ref(`app/sections/${currentSection.sid}`).update({
-            additional_resourses: [...currentSection.additional_resourses, resoursesInfo]
-        }).then(() => {
-            return dispatch({ type: ADD_ADDITIONAL_RESOURSE, payload: resoursesInfo });
-        }).catch((err) => {
-            return err;
-        });
-    };
-};
-
-export const addPlanOfAttack = (plan_of_attack) => {
-    /*   return function to redux-thunk */
-    return (dispatch, getState, { getFirebase }) => {
-        /* asyn call to firbase  to add course object with id to courses */
-        const store = getState();
-        const app = store.firebase.data.app;
-        const sid = store.sections.current_section;
-        const currentSection = app.sections[sid];
-        const firebase = getFirebase();
-        firebase.database().ref(`app/sections/${currentSection.sid}`).update({
-            ...currentSection, plan_of_attack
-        }).then(() => {
-            return dispatch({ type: ADD_PLAN_OF_ATTACT, payload: plan_of_attack });
-        }).catch((err) => {
-            return err;
-        });
-    };
-};
-
-export const addSectionToStore = (section) => {
-    return (dispatch, getState, { getFirebase }) => {
-        const store = getState();
-        const app = store.firebase.data.app;
-        const cid = store.courses.current_course;
-        const currentCourse = app.courses[cid];
-        const sid = `${cid}_${section.title}`;
-        section = {
-            ...section,
-            cid,
-            sid,
-            resourses: [''],
-            additional_resourses: [''],
-            plan_of_attack: ''
         };
-        const firebase = getFirebase();
-        firebase.database().ref(`app/courses/${cid}`).update({
-            section: [...currentCourse.section, sid]
-        }).then(() => {
-            dispatch({ type: ADD_SECTION_ID, payload: section.sid });
-            dispatch({ type: ADD_SECTION, payload: section });
-        }).catch((err) => {
-            return err;
-        });
-
     };
-};
 
-export const selectSection = (section_id) => {
-    return ({ type: ADD_SECTION_ID, payload: section_id });
-};
-export const addVideoToStore = (videoInfo) => {
-    return ({ type: ADD_VIDEO, payload: videoInfo });
-};
 
-export const addAdditionalResourseToStore = (resoursesInfo) => {
-    return ({ type: ADD_ADDITIONAL_RESOURSE, payload: resoursesInfo });
-};
 
-export const addPlanOfAttackToStore = (plan_of_attack) => {
-    return ({ type: ADD_PLAN_OF_ATTACT, payload: plan_of_attack });
-};
-
-export const submitSection = () => {
-
-    /*   return function to redux-thunk */
-    return (dispatch, getState, { getFirebase }) => {
-        /* asyn call to firbase  to add section object with id to sections */
-        const store = getState();
-        const sid = store.sections.sections.current_section;
-        const section = store.sections.sections[sid];
-        const firebase = getFirebase();
-        firebase.database().ref(`app/sections/${section.sid}`).set(section)
-            .then(() => {
-                return dispatch({ type: SUBMIT_SECTION });
+    export const addAdditionalResourse = (resoursesInfo) => {
+        /*   return function to redux-thunk */
+        return (dispatch, getState, { getFirebase }) => {
+            /* asyn call to firbase  to add course object with id to courses */
+            const store = getState();
+            const app = store.firebase.data.app;
+            const sid = store.sections.current_section;
+            const currentSection = app.sections[sid];
+            const firebase = getFirebase();
+            firebase.database().ref(`app/sections/${currentSection.sid}`).update({
+                additional_resourses: [...currentSection.additional_resourses, resoursesInfo]
+            }).then(() => {
+                return dispatch({ type: ADD_ADDITIONAL_RESOURSE, payload: resoursesInfo });
             }).catch((err) => {
-                return dispatch({ type: SUBMIT_SECTION_ERROR, payload: err });
+                return err;
             });
+        };
     };
-};
+
+    export const addPlanOfAttack = (plan_of_attack) => {
+        /*   return function to redux-thunk */
+        return (dispatch, getState, { getFirebase }) => {
+            /* asyn call to firbase  to add course object with id to courses */
+            const store = getState();
+            const app = store.firebase.data.app;
+            const sid = store.sections.current_section;
+            const currentSection = app.sections[sid];
+            const firebase = getFirebase();
+            firebase.database().ref(`app/sections/${currentSection.sid}`).update({
+                ...currentSection, plan_of_attack
+            }).then(() => {
+                return dispatch({ type: ADD_PLAN_OF_ATTACT, payload: plan_of_attack });
+            }).catch((err) => {
+                return err;
+            });
+        };
+    };
+
+    export const addSectionToStore = (section) => {
+        return (dispatch, getState, { getFirebase }) => {
+            const store = getState();
+            const app = store.firebase.data.app;
+            const cid = store.courses.current_course;
+            const currentCourse = app.courses[cid];
+            const sid = `${cid}_${section.title}`;
+            section = {
+                ...section,
+                cid,
+                sid,
+                resourses: [''],
+                additional_resourses: [''],
+                plan_of_attack: ''
+            };
+            const firebase = getFirebase();
+            firebase.database().ref(`app/courses/${cid}`).update({
+                section: [...currentCourse.section, sid]
+            }).then(() => {
+                dispatch({ type: ADD_SECTION_ID, payload: section.sid });
+                dispatch({ type: ADD_SECTION, payload: section });
+            }).catch((err) => {
+                return err;
+            });
+
+        };
+    };
+
+    export const selectSection = (section_id) => {
+        return ({ type: ADD_SECTION_ID, payload: section_id });
+    };
+    export const addVideoToStore = (videoInfo) => {
+        return ({ type: ADD_VIDEO, payload: videoInfo });
+    };
+
+    export const addAdditionalResourseToStore = (resoursesInfo) => {
+        return ({ type: ADD_ADDITIONAL_RESOURSE, payload: resoursesInfo });
+    };
+
+    export const addPlanOfAttackToStore = (plan_of_attack) => {
+        return ({ type: ADD_PLAN_OF_ATTACT, payload: plan_of_attack });
+    };
+
+    export const submitSection = () => {
+
+        /*   return function to redux-thunk */
+        return (dispatch, getState, { getFirebase }) => {
+            /* asyn call to firbase  to add section object with id to sections */
+            const store = getState();
+            const sid = store.sections.sections.current_section;
+            const section = store.sections.sections[sid];
+            const firebase = getFirebase();
+            firebase.database().ref(`app/sections/${section.sid}`).set(section)
+                .then(() => {
+                    return dispatch({ type: SUBMIT_SECTION });
+                }).catch((err) => {
+                    return dispatch({ type: SUBMIT_SECTION_ERROR, payload: err });
+                });
+        };
+    };
