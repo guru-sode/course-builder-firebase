50b79f3b99b4d8d759104ad53b132a92a1695ca6
guru-sode <gurukiran.sd@mountblue.io>
2018-10-19 11:34:30 +0530

    Add resources feedback

--- src/components/addResources.jsx
+++ src/components/addResources.jsx
@@ -6,16 +6,35 @@ import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { addAdditionalResourseToStore } from '../redux/actions/sectionActions';
+import { Grid, Typography, Dialog, DialogTitle, DialogContent, CardContent, Card } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
+    // container: {
+    //     display: 'flex',
+    //     flexWrap: 'wrap',
+    // },
+    // textField: {
+    //     marginLeft: theme.spacing.unit,
+    //     marginRight: theme.spacing.unit,
+    //     width: '100%',
+    // },
+    // button: {
+    //     marginLeft: '45%',
+    //     marginTop: '2%',
+    //     backgroundColor: '#000a12',
+    //     color: 'white',
+    //     '&:hover': {
+    //         backgroundColor: '#000a12'
+    //     }
+    // },
     container: {
-        display: 'flex',
-        flexWrap: 'wrap',
+        margin: '2em',
+        width: '100%'
     },
     textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: '100%',
+        width: '55%'
     },
     button: {
         marginLeft: '45%',
@@ -26,86 +45,194 @@ const styles = theme => ({
             backgroundColor: '#000a12'
         }
     },
+    expandDiv: {
+        width: '100%'
+    },
+    emptyGrid: {
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    buttons: {
+        backgroundColor: '#000a12',
+        color: 'white',
+        '&:hover': {
+            backgroundColor: '#000a12'
+        },
+        marginLeft: '0.25em',
+        marginBottom: '1em'
+    },
+    buttonGroupDialog: {
+        display: 'flex',
+        justifyContent: 'center',
+        textAlign: 'start',
+        margin: '0.3em 0.3em 0 0'
+    },
+    dialogBox: {
+        padding: '0.25em',
+        margin: 'auto',
+        textAlign: 'center',
+    },
+    progress: {
+        margin: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: '#000a12'
+    },
+    videos: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    videoContainer: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+    }
 });
 
 class AddResources extends Component {
+
     constructor(props) {
         super(props);
+        this.state = {
+            open: false,
+            title: '',
+            description: '',
+            url: ''
+        };
         this.handleAdd = this.handleAdd.bind(this);
     }
-
     handleAdd(e) {
         e.preventDefault();
-        const name = document.getElementById('name').value;
-        const url = document.getElementById('description').value;
-        const description = document.getElementById('url').value;
-        const resoursesInfo = {
-            name,
-            description,
-            url,
+        const videoInfo = {
+            name: this.state.title,
+            description: this.state.description,
+            url: this.state.url
         };
-        if(name!==''&&url!==''&&description!=='')
-            this.props.addAdditionalResourseToStore(resoursesInfo);
-        document.getElementById('name').value = '';
+        if (this.state.title !== '' && this.state.description !== '' && this.state.url !== '')
+            this.props.addAdditionalResourseToStore(videoInfo);
+        document.getElementById('title').value = '';
         document.getElementById('description').value = '';
         document.getElementById('url').value = '';
-
-
     }
 
-    componentWillUnmount() {
-        const name = document.getElementById('name').value;
-        const url = document.getElementById('url').value;
-        const description = document.getElementById('description').value;
-        const resoursesInfo = {
-            name,
-            description,
-            url,
-        };
-        if(name!==''&&url!==''&&description!=='')
-            this.props.addAdditionalResourseToStore(resoursesInfo);
+    handleDialogOpen = () => {
+        this.setState({ open: true });
+    }
+
+    handleDialogClose = () => {
+        this.setState({ open: false });
     }
 
     render() {
-        const { classes } = this.props;
-
+        const { classes, section } = this.props;
+        console.log(this.state.title)
         return (
-            <form className={classes.container} id="titleForm" noValidate autoComplete="off">
-                <TextField
-                    id="name"
-                    label="Resource Name"
-                    className={classes.textField}
-                    type="name"
-                    name="name"
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="description"
-                    multiline={true}
-                    label="Description"
-                    className={classes.textField}
-                    type="description"
-                    name="description"
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    id="url"
-                    multiline={true}
-                    label="Resource URL"
-                    className={classes.textField}
-                    type="url"
-                    name="url"
-                    margin="normal"
-                    variant="outlined"
-                />
-                <Button onClick={this.handleAdd} variant="contained" className={classes.button} align="end">Add another URL</Button>
-            </form>
+            <div className={classes.container}>
+                <Dialog
+                            className={classes.dialogBox}
+                            open={this.state.open}
+                            onClose={this.handleDialogClose}
+                        >
+                            <DialogTitle id="form-dialog-title" className={classes.titleAlign}>
+                                Add videos
+                            </DialogTitle>
+                            <DialogContent>
+                                <TextField
+                                    id="title"
+                                    label="Resource title"
+                                    className={classes.textField}
+                                    type="title"
+                                    name="title"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={(e) => { this.setState({title: e.target.value}) }}
+                                />
+                                <TextField
+                                    id="description"
+                                    multiline={true}
+                                    label="Resource description"
+                                    className={classes.textField}
+                                    type="description"
+                                    name="description"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={(e) => { this.setState({description: e.target.value}) }}
+                                />
+                                <TextField
+                                    id="url"
+                                    multiline={true}
+                                    label="Resource URL"
+                                    className={classes.textField}
+                                    type="url"
+                                    name="url"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={(e) => { this.setState({url: e.target.value}) }}
+                                />
+                                <Grid item className={classes.buttonGroupDialog}>
+                                    <Button onClick={this.handleAdd} variant="contained" className={classes.buttons} align="end">Add</Button>
+                                    <Button onClick={this.handleDialogClose} variant="contained" className={classes.buttons} align="end">Cancel</Button>
+                                </Grid>
+                            </DialogContent>
+                        </Dialog>
+
+                {section !== undefined ?
+                    <Grid container>
+                        <Grid className={classes.videos} container>
+                        {section.additional_resourses.length !== 1 ? 
+                            <Button variant="fab" onClick={this.handleDialogOpen} mini className={classes.buttons} aria-label="Add">
+                                <AddIcon />
+                            </Button>: false}
+                        <Grid container className={classes.videoContainer}>
+                        {section.additional_resourses.length !== 1 ?
+                            section.additional_resourses.map((video, index) => {
+                                if(index !== 0) {
+                                    return (
+                                        <Grid item>
+                                        <Card>
+                                            <CardContent>
+                                                <CardContent>
+                                                    <Typography variant="h6">
+                                                        {video.name}    
+                                                    </Typography>
+                                                    <Typography component="p">
+                                                        {video.description}
+                                                    </Typography>
+                                                    <a href={video.url} target="_blank" style={{ textDecoration: 'none' }}>{video.url}</a>
+                                                </CardContent>
+                                            </CardContent>
+                                        </Card>
+                                        </Grid>
+                                    );
+                                }
+                            }) :
+                            <Grid className={classes.emptyGrid} container>
+                                <Grid item>
+                                    <Typography variant="h6">No Resources</Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="fab" onClick={this.handleDialogOpen} mini className={classes.buttons} aria-label="Add">
+                                        <AddIcon />
+                                    </Button>
+                                </Grid>
+                            </Grid>}
+                            </Grid>
+                        </Grid>
+                    </Grid> :
+                    <Grid className={classes.progressContainer} container><CircularProgress className={classes.progress} size={100} /> </Grid>
+                    }
+            </div>
         );
     }
 }
 
+const mapStateToProps = state => {
+    const sid = state.sections.current_section;
+    const sections = state.sections.sections;
+    const section = sections ? sections[sid] : null;
+    return {
+        /* getting data from firebase redux store { firebaseReducer as firebase } */
+        section: section,
+    };
+};
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -115,5 +242,5 @@ const mapDispatchToProps = dispatch => {
 
 export default compose(
     withStyles(styles),
-    connect(null, mapDispatchToProps),
+    connect(mapStateToProps, mapDispatchToProps),
 )(AddResources);
